6e95f694e5c723738bbc0fcadb30400f6cab05e3
jFrostyBoy <jfrostyboy01@gmail.com>
2025-11-07 17:46:40 +0200

    Refactor LiveCommand for jlivestream package

--- src/main/java/jfbdev/jLive/Commands/LiveCommand.java
+++ src/main/java/jfbdev/jLive/Commands/LiveCommand.java
@@ -1,5 +1,9 @@
-package jfbdev.jLive.Commands;
+package jfbdev.jlivestream.commands;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -9,12 +13,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 public class LiveCommand implements CommandExecutor {
@@ -27,66 +28,88 @@ public class LiveCommand implements CommandExecutor {
         this.cooldown = new HashMap<>();
     }
 
-    @Deprecated
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
 
-        String prefix = plugin.getConfig().getString("prefix", ".");
+        String prefix = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("prefix", ""));
 
-        String soundName = plugin.getConfig().getString("sound.name", "ENTITY_PLAYER_LEVELUP");
-        float volume = (float) plugin.getConfig().getDouble("sound.volume", 1.0);
-        float pitch = (float) plugin.getConfig().getDouble("sound.pitch", 1.0);
+        String soundName = plugin.getConfig().getString("sound-start.name", "ENTITY_PLAYER_LEVELUP");
+        float volume = (float) plugin.getConfig().getDouble("sound-start.volume", 1.0);
+        float pitch = (float) plugin.getConfig().getDouble("sound-start.pitch", 2.0);
 
         if (!(commandSender instanceof Player player)) {
-            commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.console", ".")));
+            commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.only-players", "")));
             return true;
         }
-        if (!player.hasPermission("jlive.use")) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.no-permission", ".")));
+        if (!player.hasPermission("jlivestream.live")) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.no-permission", "")));
             return true;
         }
+
+        if (player.hasPermission("jlivestream.online")) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.already-live", "")));
+            return true;
+        }
+
         if (args.length == 0) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.usage", ".")));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.live-usage", "")));
             return true;
         }
+
         if (args.length > 1) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.usage", ".")));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.live-usage", "")));
             return true;
         }
+
         String link = args[0];
         if (!link.matches("^(https?://).+")) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.link-format", ".")));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.invalid-link", "")));
             return true;
         }
-        long cooldownTime = plugin.getConfig().getLong("cooldown", 60) * 1000;
+
+        long cooldownTime = plugin.getConfig().getLong("cooldown-time", 60) * 1000;
         UUID playerUUID = player.getUniqueId();
         long currentTime = System.currentTimeMillis();
         long lastUsed = cooldown.getOrDefault(playerUUID, 0L);
 
         if (lastUsed != 0 && currentTime < lastUsed + cooldownTime) {
             long timeLeft = (lastUsed + cooldownTime - currentTime) / 1000;
-            String cooldownMessage = plugin.getConfig().getString("messages.cooldown-message", ".");
+            String cooldownMessage = plugin.getConfig().getString("messages.cooldown-message", "");
             cooldownMessage = cooldownMessage.replace("%cooldown%", String.valueOf(timeLeft));
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + cooldownMessage));
             return true;
         }
+
         cooldown.put(playerUUID, currentTime);
-        String linkMessageConfig = plugin.getConfig().getString("messages.live-message", ".");
-        String formattedMessage = linkMessageConfig
-                .replace("%link%", link)
-                .replace("%player%", player.getName());
+        List<String> lines = plugin.getConfig().getStringList("broadcast.live.started");
+        Component finalMessage = Component.empty();
 
-        Component message = LegacyComponentSerializer.legacyAmpersand().deserialize(formattedMessage);
-        Component clickableLink = Component.text(link)
-                .color(NamedTextColor.BLUE)
-                .clickEvent(ClickEvent.openUrl(link));
+        for (String line : lines) {
+            String formatted = line
+                    .replace("%player%", player.getName())
+                    .replace("%link%", link);
 
-        message = message.replaceText(builder -> builder
-                .matchLiteral(link)
-                .once()
-                .replacement(clickableLink)
-        );
-        plugin.getServer().broadcast(message);
+            Component component = LegacyComponentSerializer.legacyAmpersand().deserialize(formatted);
+
+            if (line.contains("%link%")) {
+                component = component.replaceText(builder -> builder
+                        .matchLiteral(link)
+                        .once()
+                        .replacement(
+                                Component.text(link)
+                                        .color(NamedTextColor.BLUE)
+                                        .clickEvent(ClickEvent.openUrl(link))
+                        )
+                );
+            }
+            if (finalMessage != Component.empty()) {
+                finalMessage = finalMessage.append(Component.newline());
+            }
+            finalMessage = finalMessage.append(component);
+        }
+        plugin.getServer().broadcast(finalMessage);
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + plugin.getConfig().getString("messages.live-started", "")));
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " permission set jlivestream.online");
 
         Sound sound = Sound.valueOf(soundName.toUpperCase());
         for (Player online : Bukkit.getOnlinePlayers()) {
